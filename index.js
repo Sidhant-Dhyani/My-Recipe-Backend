ffb6d738b7ca68144efc3716369c992dfbf06613
sidhant dhyani <siddhantydhyani99@gmail.com>
2024-01-01 21:02:14 +0530

    Auth done.

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+
 const cors = require("cors");
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(cors());
+const mongoose = require("mongoose");
+
+const authRoute = require('./routes/auth');
 
 const port = 4000;
+const atlas =
+    "mongodb+srv://siddhantydhyani99:c7XGZ6IRCV949LWs@cluster0.7pdjkrf.mongodb.net/Recipe";
+
+const connectToDB = async (req, res) => {
+    try {
+        await mongoose.connect(atlas);
+        console.log("Connected to MongoDB Atlas!!");
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+connectToDB();
+
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
-    app.get("/", (req, res) => {
-        res.send("Sidhant's Kitchen Backend is Live!!");
-    });
+    res.send("Sidhant's Kitchen Backend is Live!!");
+});
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}!!!`);
 });
